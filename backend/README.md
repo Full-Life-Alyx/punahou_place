83b8c7fcb2648a62f7862dfe422b08723934462b
Full-Life-Alyx <96643025+Full-Life-Alyx@users.noreply.github.com>
2025-11-01 21:27:09 -1000

    Initial

---
+++ backend/README.md
@@ -0,0 +1,24 @@
+# punahou_place
+
+[![Package Version](https://img.shields.io/hexpm/v/punahou_place)](https://hex.pm/packages/punahou_place)
+[![Hex Docs](https://img.shields.io/badge/hex-docs-ffaff3)](https://hexdocs.pm/punahou_place/)
+
+```sh
+gleam add punahou_place@1
+```
+```gleam
+import punahou_place
+
+pub fn main() -> Nil {
+  // TODO: An example of the project in use
+}
+```
+
+Further documentation can be found at <https://hexdocs.pm/punahou_place>.
+
+## Development
+
+```sh
+gleam run   # Run the project
+gleam test  # Run the tests
+```
---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Gleam Erlang dev environment";
+  inputs = {nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";};
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    allSystems = ["x86_64-linux" "aarch64-darwin"];
+    forAllSystems = fn:
+      nixpkgs.lib.genAttrs allSystems
+      (system: fn {pkgs = import nixpkgs {inherit system;};});
+  in {
+    devShells = forAllSystems ({pkgs}: {
+      default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          gleam
+          erlang
+          rebar3
+          beam27Packages.elixir
+          beam27Packages.hex
+        ];
+      };
+    });
+  };
+}
